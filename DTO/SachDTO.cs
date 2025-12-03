fadf4412da4fa0121a9ec173b96561e7c039b2dc
lekhanhhoang <lekhanhhoang139@gmail.com>
2025-12-03 18:31:02 +0700

    Update SachDTO.cs

---
+++ DTO/SachDTO.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace DTO
+{
+    public class SachDTO
+    {
+        public int MaSach { get; set; }
+        public string TieuDe { get; set; }
+        public string ISBN { get; set; }
+        public int NamXuatBan { get; set; }
+        public decimal GiaSach { get; set; }
+        public int SoLuongTong { get; set; }
+        public int SoLuongCon { get; set; }
+        public int MaNXB { get; set; }
+        public int MaTheLoai { get; set; }
+
+        public SachDTO()
+        {
+            MaSach = 0;
+            TieuDe = string.Empty;
+            ISBN = string.Empty;
+            NamXuatBan = DateTime.Now.Year;
+            GiaSach = 0;
+            SoLuongTong = 0;
+            SoLuongCon = 0;
+            MaNXB = 0;
+            MaTheLoai = 0;
+        }
+
+        public SachDTO(int maSach, string tieuDe, string isbn, int namXuatBan, decimal giaSach,
+                       int soLuongTong, int soLuongCon, int maNXB, int maTheLoai)
+        {
+            MaSach = maSach;
+            TieuDe = tieuDe;
+            ISBN = isbn;
+            NamXuatBan = namXuatBan;
+            GiaSach = giaSach;
+            SoLuongTong = soLuongTong;
+            SoLuongCon = soLuongCon;
+            MaNXB = maNXB;
+            MaTheLoai = maTheLoai;
+        }
+    }
+}
