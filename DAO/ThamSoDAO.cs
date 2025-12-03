f0297868aa2c08536e9d8999e8f06bbe83c74a98
lekhanhhoang <lekhanhhoang139@gmail.com>
2025-12-03 23:12:08 +0700

    sach

---
+++ DAO/ThamSoDAO.cs
@@ -0,0 +1,11 @@
+namespace DAO
+{
+    public class ThamSoDAO
+    {
+        public int LayKhoangCachXuatBan()
+        {
+            // Return a default value or implement as needed
+            return 5;
+        }
+    }
+}
